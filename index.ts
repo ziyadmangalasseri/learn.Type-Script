77165c8bd94ba4bd0e253538df3b40d17f19704d
ziyad <ziyadmuhammad9696@gmail.com>
2025-02-25 15:07:34 +0530

    git ignore added

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 // Infer Type (Implicit Type)
-let usersName = "ziyad"
-usersName = 10
+
+// let usersName = "ziyad"
+// usersName = 10
+
+
+
 
 // Defaining Type(Explicit Type)
+
+// let fulName: string = "Muhammed ziyad";
+// let age: number = 17;
+// let softwareEngineer: boolean = true;
+// let skills: string[] = ["react", "next", "express", "mongodb"];
+// let count: number[] = [1, 2, 3, 4];
+// let emtyArray: [] = [];
+// let userDetails: {
+//   name: string;
+//   age: number;
+//   place: string;
+//   skilled: boolean;
+// } = { name: "ziyad", age: 17, place: "Malappuram", skilled: true };
